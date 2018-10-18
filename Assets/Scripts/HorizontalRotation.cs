2f14fef6a43b97a5917a82c90c2bfa1fc6e401b0
Charl Damon PAUWELS <cpauwels@e4r6p11.wethinkcode.co.za>
2018-10-18 09:32:42 +0200

    Initial Commit

---
+++ Assets/Scripts/HorizontalRotation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalRotation : MonoBehaviour {
+
+	public float rotationSpeed = 10f;
+	[HideInInspector] public float maxRange = 100f;
+	[HideInInspector] public float minRange = 85f;
+
+	float horizontal;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontal = Input.GetAxis("Mouse X");
+		var mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Input.GetKey(KeyCode.Mouse0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(mousePosition, out hit))
+				RotateObject();
+		}
+		CheckIfFinished();
+	}
+
+	void RotateObject()
+	{
+		transform.Rotate(transform.up * horizontal * rotationSpeed);
+	}
+
+	public bool CheckIfFinished()
+	{
+		if (!Input.GetKey(KeyCode.Mouse0))
+		{
+			if (transform.eulerAngles.y >= minRange && transform.eulerAngles.y <= maxRange)
+			{
+				Debug.Log("Level Finished");	
+			}
+		}
+		return false;
+	}
+}
---
+++ Assets/Scripts/VerticalRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalRotation : MonoBehaviour {
+
+	public float rotationSpeed = 10f;
+
+	float vertical;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vertical = Input.GetAxis("Mouse Y");
+		var mousePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Input.GetKey(KeyCode.Mouse0) && Input.GetKey(KeyCode.LeftControl))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(mousePosition, out hit))
+				RotateObject();
+		}
+	}
+
+	void RotateObject()
+	{
+		transform.Rotate(transform.forward * vertical * rotationSpeed);
+	}
+}
